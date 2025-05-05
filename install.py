0c5f26fd64fc4611c8b047df1ffd8f5af0a08967
Mingyuan <mxia9416@uni.sydney.edu.au>
2025-05-05 10:03:54 +1000

    保留requirements.txt

--- install.py
+++ install.py
@@ -60,14 +60,11 @@ def install_resource():
 
 
 def install_chores():
-    shutil.copy2(
-        working_dir / "README.md",
-        install_path,
-    )
-    shutil.copy2(
-        working_dir / "LICENSE",
-        install_path,
-    )
+    for file in ["README.md", "LICENSE", "requirements.txt"]:
+        shutil.copy2(
+            working_dir / file,
+            install_path,
+        )
 
 def install_agent():
     shutil.copytree(
